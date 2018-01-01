591f95f59423d1b025521b192d21bc440ee766d3
Johan Vromans <jvromans@squirrel.nl>
2018-01-01 19:17:12 +0100

    Update ownCloud to 10.0.4 and PHP 5.6

--- spk/owncloud/src/owncloud.sh
+++ spk/owncloud/src/owncloud.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PHP="php -d open_basedir=none -f"
+PHP="/usr/local/bin/php56 -d open_basedir=none -d extension=zip.so -d extension=gd.so -d extension=curl.so -d extension=posix.so -f"
 OWNCLOUD="/var/services/web/owncloud/cron.php"
 SLEEP_TIME="1800"
 
